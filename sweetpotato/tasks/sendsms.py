66c070d38da5e6e07e3c231601ef9528009eeba5
Dmytri Kleiner <dk@telekommunisten.net>
2009-02-10 13:35:41 +0100

    added exception handling to sendsms

--- sweetpotato/tasks/sendsms.py
+++ sweetpotato/tasks/sendsms.py
@@ -1,16 +1,23 @@
 from sweetpotato.core import TaskAdapter
 """ send an sms
 """
+import logging
 class sendsms(TaskAdapter):
     """ send an sms
     """
     def run(self):
         to = self.task.getProperty('to') 
         text = self.task.getProperty('text') 
+        message = {'Text': text, 'SMSC': {'Location': 1}, 'Number': to}
         import gammu
         sm = gammu.StateMachine()
-        sm.ReadConfig()
-        sm.Init()
-        message = {'Text': text, 'SMSC': {'Location': 1}, 'Number': to}
-        sm.SendSMS(message)
+        try:
+            sm.ReadConfig()
+            sm.Init()
+            sm.SendSMS(message)
+        except Exception as errors:
+            for e in errors.args:
+                self.task.log("Error %s in %s: %s" % (e["Code"],e["Where"],e["Text"]), logging.ERROR)
+            raise SystemExit  
+          
 
